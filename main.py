dc832d8d11a85c548eddee288942d555561daccc
Alex <alex.isaac328@gmail.com>
2019-03-26 23:08:55 -0600

    Added the ability for planes to turn. Added some comments. Started the calculations of avoiding collisions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from plane import Plane
+import math
 
 
 WINDOWWIDTH = 740
@@ -23,7 +24,8 @@ def main():
 
 
 def runSimulation():
-	planes = [Plane(100, 100, 300, 1)]
+	# the current planes in the simulation
+	planes = [Plane(100, 100, 0, 1), Plane(400, 300, 240, 1)]
 	while True:
 		# go through events
 		for event in pygame.event.get(): # event handling loop
@@ -32,12 +34,47 @@ def runSimulation():
 			elif event.type == KEYDOWN:
 				if event.key == K_ESCAPE:
 					return True
-
+				# testing
+				if event.key == K_t:
+					planes[0].turn(40)
 
 		DISPLAYSURF.fill((0,0,0))
+
+		for plane1 in planes:
+			for plane2 in planes:
+				
+				if plane1 is plane2:
+					continue
+				
+				dist = math.sqrt((plane1.x - plane2.x)**2 + (plane1.y - plane2.y)**2)
+				if dist <= plane1.exclusion_zone * 1.2:
+					x_diff = plane1.x - plane2.x
+					y_diff = plane1.y - plane2.y
+
+					# if plane 2 is to the left and above plane 1
+					if x_diff > 0 and y_diff > 0:
+						pass
+					# if plane 2 is to the right and above plane 1
+					elif x_diff < 0 and y_diff > 0:
+						pass
+					# if plane 2 is to the left and below plane 1
+					elif x_diff > 0 and y_diff < 0:
+						pass
+					# if plane 2 is to the right and below plane 1
+					elif x_diff < 0 and y_diff < 0:
+						pass
+
+					
+
+		
 		for plane in planes:
 			plane.move()
+			pygame.draw.circle(DISPLAYSURF, (226, 57, 31), (int(plane.x), int(plane.y)), plane.exclusion_zone)
 			pygame.draw.circle(DISPLAYSURF, (255, 255, 255), (int(plane.x), int(plane.y)), 2)
+
+
+
+
 		pygame.display.update()
 		FPSCLOCK.tick(FPS)
 
--- plane.py
+++ plane.py
@@ -1,18 +1,47 @@
 import math
 
+
+# class representation of a plane
 class Plane:
+	# initialize the plane
+	# ------------ Parameters ----------------------
+	# x       : x-coordinate of the plane
+	# y       : y-coordinate of the plane
+	# heading : current heading (direction) of the plane. Ranges from 0-360. 0 is directly right
+	# speed   : speed of the plane. currently the only value that works well is 1
+	#
+	# ----------- Other Values ------------------------
+	# turning_degrees : The number of degrees the plane needs to turn. Default value is 0
+	# exclusion_zone  : The area in which no other planes are allowed to enter. 
+	#       For example, if exclusion_zone is set at 50, no other planes can come within 50 pixels of this plane
 	def __init__(self, x, y, heading, speed):
 		self.x = x
 		self.y = y
-		self.heading = math.radians(heading)
+		self.heading = heading
 		self.speed = speed
+		self.turning_degrees = 0
+		self.exclusion_zone = 100
 
+	# move the plane. executes every frame. If the plane needs to turn (turning_degrees != 0) that happens as well
 	def move(self):
-		self.x = self.x + (self.speed * math.cos(self.heading))
-		self.y = self.y + (self.speed * math.sin(self.heading))
+		# check if the plane needs to turn
+		if self.turning_degrees > 0:
+			self.heading += 1
+			self.turning_degrees -= 1
+		elif self.turning_degrees < 0:
+			self.heading -= 1
+			self.turning_degrees += 1
+
+		# calculate the new coordinates of the plane
+		self.x = self.x + (self.speed * math.cos(math.radians(self.heading)))
+		self.y = self.y + (self.speed * math.sin(math.radians(self.heading)))
 
 	def draw(self):
 		pass
+
+	# turn the plane. values range from [-180, 180]. Positive values turn right, negative values left
+	def turn(self, degrees):
+		self.turning_degrees = degrees
 	
 
 	
